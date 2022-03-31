d5b552dd4193b6c0a3d6747567d05bc85c59b818
Fernando A. Uribe <fauribe56@gmail.com>
2022-03-30 20:51:42 -0700

    Updated components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView} from '../movie-view/movie-view';
 
@@ -7,13 +9,21 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-          { _id: 1, Title: 'Star Wars Episode IV: A New Hope', Description: 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire\'s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.', ImagePath: '../../assets/sw4.jpeg', Genre: 'Sci-Fi', Director: 'George Lucas'},
-          { _id: 2, Title: 'INSIDE', Description: 'A musical comedy special shot and performed by Bo Burnham, alone, over the course of a very unusual year.', ImagePath: '../../assets/inside.jpg', Genre: 'Comedy', Director: 'Bo Burnham'},
-          { _id: 3, Title: 'COCO', Description: 'Aspiring musician Miguel, confronted with his family\'s ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.', ImagePath: '../../assets/coco.jpg', Genre: 'Family', Director: 'Adrian Molina'},
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount(){
+    axios.get('https://fernando-myflix-3.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
