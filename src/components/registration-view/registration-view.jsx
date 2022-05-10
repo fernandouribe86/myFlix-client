d671f29a27265d6113d194ab5d629d2bca64ed36
Fernando A. Uribe <fauribe56@gmail.com>
2022-05-09 21:49:38 -0700

    updated Registration View

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'React';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
